dbb5ae88023d7369acf2521171d67bdb83ce9770
Ashish Kumar <80582252+ashishok@users.noreply.github.com>
2021-10-19 17:08:01 +0530

    Add files via upload

---
+++ Java/Hello_world.java
@@ -0,0 +1,8 @@
+
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.print("Hello Java This is My First Java Program");
+        System.out.print(" loved it");
+    }
+}
---
+++ Java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        int a=1 ,b=2,c=3;
+        int d = a+b+c;
+        System.out.print("Answer is"+d);
+    }
+}
