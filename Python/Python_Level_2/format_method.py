04984f0b72f3d67741f26598ff36c9a2be4da4a5
Ashish Kumar <80582252+ashishok@users.noreply.github.com>
2021-10-19 17:01:46 +0530

    Add files via upload

---
+++ Python/Python_Level_2/format_method.py
@@ -0,0 +1,6 @@
+name = 'ashish'
+college = "maharaja agrasen institute of technology"
+
+
+a = "my name is {} and my college name is {} ".format(name,college)
+print(a)
---
+++ Python/Python_Level_2/py.py
@@ -0,0 +1,4 @@
+# .\project_ashish\Scripts\activate.ps1
+# import flask
+# import pandas as pd
+# import pygame
