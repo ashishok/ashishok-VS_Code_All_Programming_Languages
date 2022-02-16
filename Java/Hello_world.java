68f7ffa13658e24f483b3ce5c6776e1c339f7bb1
Ashish Kumar <80582252+ashishok@users.noreply.github.com>
2022-02-16 21:52:08 +0530

    Delete Java directory

--- Java/Hello_world.java
+++
@@ -1,8 +0,0 @@
-
-public class Hello_world {
-
-    public static void main(String[] args) {
-        System.out.print("Hello Java This is My First Java Program");
-        System.out.print(" loved it");
-    }
-}
--- Java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        int a=1 ,b=2,c=3;
-        int d = a+b+c;
-        System.out.print("Answer is"+d);
-    }
-}
